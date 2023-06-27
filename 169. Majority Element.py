fab1ebe25448e6f11f6ea6e85df276ff94ddfe6d
oerzn <harshinprivate@gmail.com>
2023-06-27 23:44:34 +0530

    169. Majority Element

---
+++ 169. Majority Element.py
@@ -0,0 +1,14 @@
+#169. Majority Element
+#https://leetcode.com/problems/majority-element/
+
+class Solution:
+  def majorityElement(self, nums: List[int]) -> int:
+    ans = None
+    count = 0
+
+    for num in nums:
+      if count == 0:
+        ans = num
+      count += (1 if num == ans else -1)
+
+    return ans
